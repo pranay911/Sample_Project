f8cf2a191d9506f06195cc96886b6813f03f6721
Pranay Gaikwad <65008142+pranay911@users.noreply.github.com>
2025-03-03 17:48:28 +0530

    New File Added

---
+++ python2.py
@@ -0,0 +1,15 @@
+# Simple addition in Python 2.x
+
+# Get input from the user
+num1 = raw_input("Enter the first number: ")
+num2 = raw_input("Enter the second number: ")
+
+# Convert input to integers
+num1 = int(num1)
+num2 = int(num2)
+
+# Add the numbers
+sum = num1 + num2
+
+# Print the result
+print "The sum of", num1, "and", num2, "is", sum
